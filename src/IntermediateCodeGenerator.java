0ba846ee0b68df01fece3831b157ba69901375cf
Mohamed Fathy <mofasa.peace@gmail.com>
2022-04-24 20:41:14 +0200

    if

--- src/IntermediateCodeGenerator.java
+++ src/IntermediateCodeGenerator.java
@@ -6,8 +6,31 @@ public class IntermediateCodeGenerator extends JavaParserBaseListener {
 
     public IntermediateCodeGenerator (TokenStreamRewriter rewriter) {this.rewriter = rewriter;}
 
+    private String getBlock() {return "FileWrite.Singleton().append(" + blockNumber++ + ");\n";}
+
     @Override
     public void enterBlock(JavaParser.BlockContext ctx) {
-        rewriter.insertBefore(ctx.blockStatement(0).start, "FileWrite.Singleton().append(" + blockNumber++ + ");\n");
+        rewriter.insertBefore(ctx.blockStatement(0).start, getBlock());
+    }
+
+    @Override
+    public void enterStatement(JavaParser.StatementContext ctx) {
+        switch (ctx.getChild(0).getText()) {
+            case "if" : {
+                if (!ctx.statement(0).start.getText().equals("{")) {
+                   rewriter.insertBefore(ctx.statement(0).start, "{ \n" + getBlock());
+                   rewriter.insertAfter(ctx.statement(0).stop, "}");
+                }
+                if (ctx.statement(1) != null) {
+                    if (!ctx.statement(1).start.getText().equals("{")) {
+                        rewriter.insertBefore(ctx.statement(1).start, "{ \n" + getBlock());
+                        rewriter.insertAfter(ctx.statement(1).stop, "}");
+                    }
+                }
+            }
+            case "for" : {}
+            case "while" : {}
+            case "do" : {}
+        }
     }
 }
