ac329e446f46f28e4a50b146fb8fa1d15e789155
omarmohamed101 <omar.mohamed5557@gmail.com>
2022-04-27 19:20:11 +0200

    docs

--- Docs/html/search/classes_0.js
+++ Docs/html/search/classes_0.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['example1_0',['Example1',['../class_example1.html',1,'']]],
   ['example2_1',['Example2',['../class_example2.html',1,'']]],
-  ['example4_2',['Example4',['../class_example4.html',1,'']]],
-  ['example5_3',['Example5',['../class_example5.html',1,'']]],
-  ['example6_4',['Example6',['../class_example6.html',1,'']]],
-  ['example7_5',['Example7',['../class_example7.html',1,'']]]
+  ['example3_2',['Example3',['../class_example3.html',1,'']]],
+  ['example4_3',['Example4',['../class_example4.html',1,'']]],
+  ['example5_4',['Example5',['../class_example5.html',1,'']]],
+  ['example6_5',['Example6',['../class_example6.html',1,'']]],
+  ['example7_6',['Example7',['../class_example7.html',1,'']]]
 ];
